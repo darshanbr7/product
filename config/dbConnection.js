575f9367a0a37a9d6596cb3af687a0a54ba4f832
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-12-09 18:20:46 +0530

    added husky

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dbConnection = () => {
   mongoose
     .connect(
-      "mongodb+srv://darsan:123456d@cluster0.ac3q66d.mongodb.net/product"
+      "mongodb+srv://darsan:123456d@cluster0.ac3q66d.mongodb.net/product",
     )
     .then(() => {
       console.log("connected to server");
